63423774591c3afa99263282318cfa4dbbde6090
davidtom <davidjtomczyk@gmail.com>
2026-01-01 21:42:30 -0600

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import GamePage from "./pages/GamePage";
 import CalibrationPage from "./pages/CalibrationPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<GamePage />} />
-      <Route path="/calibrate" element={<CalibrationPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<GamePage />} />
+        <Route path="/calibrate" element={<CalibrationPage />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
